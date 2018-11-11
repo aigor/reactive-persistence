cc83db7b3fa4fb1fb416e8ccca6d5c197943c401
Igor Lozynskyi <igor.lozynskyi@zoomdata.com>
2018-11-11 18:34:46 +0200

    Basic application

---
+++ src/main/java/org/coinen/reactive/persistence/ReactivePersistence/ReactivePersistenceApplication.java
@@ -0,0 +1,12 @@
+package org.coinen.reactive.persistence.ReactivePersistence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactivePersistenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactivePersistenceApplication.class, args);
+	}
+}
---
+++ src/test/java/org/coinen/reactive/persistence/ReactivePersistence/ReactivePersistenceApplicationTests.java
@@ -0,0 +1,16 @@
+package org.coinen.reactive.persistence.ReactivePersistence;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ReactivePersistenceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
